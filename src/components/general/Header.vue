a68b2427618b4e24233574519fef2e7487df71af
Crepiks <sayazhan.onlassyn@mail.ru>
2020-04-28 10:05:05 +0600

    feat: create Header component's basic markup

---
+++ src/components/general/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header">
+    <logo class="header__logo" />
+    <nav class="header__nav nav">
+      <router-link to="#" class="header__link">Курсы</router-link>
+      <router-link to="#" class="header__link">Подписка</router-link>
+      <router-link to="#" class="header__link">Блог</router-link>
+    </nav>
+    <div class="header__buttons"></div>
+  </header>
+</template>
+
+<script>
+import Logo from "@/components/general/Logo";
+
+export default {
+  components: {
+    logo: Logo
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  width: 80%;
+  padding: 29px 0;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.header__link {
+  margin-right: 55px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #767ead;
+  text-decoration: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,18 @@
     <offer-bar linkUrl="/promo" linkText="Подробнее"
       >Бесплатный курс по верстке</offer-bar
     >
+    <c-header />
   </div>
 </template>
 
 <script>
 import OfferBar from "@/components/general/OfferBar";
+import Header from "@/components/general/Header";
 
 export default {
   components: {
-    "offer-bar": OfferBar
+    "offer-bar": OfferBar,
+    "c-header": Header
   }
 };
 </script>
