36153941cdd4bb61199150da147ab308ea8711ed
Sayazhan Onglassyn <sayazhan.onlassyn@mail.ru>
2020-09-23 18:20:46 +0600

    feat: update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,6 @@
 const apiUrl = process.env.VUE_APP_API_URL;
-const apiVersion = process.env.VUE_APP_API_VERSION;
 
 export default {
   apiUrl,
-  apiVersion,
-  apiBaseUrl: `${apiUrl}/api/`,
-  ymapApiKey: process.env.VUE_APP_YMAP_API_KEY
+  apiBaseUrl: `${apiUrl}/api/`
 };
