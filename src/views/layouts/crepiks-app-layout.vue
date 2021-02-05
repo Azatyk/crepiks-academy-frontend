25d12244f8b341b5d3209b7d3537dffc8fab30c6
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2021-02-06 00:39:30 +0600

    merge: auto-merging

---
+++ src/views/layouts/crepiks-app-layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-page">
+    <sidebar />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import sidebar from "@/components/app-layout/crepiks-sidebar";
+
+export default {
+  components: {
+    sidebar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/variables.scss";
+
+.app-page {
+  width: 100%;
+  min-height: 100vh;
+  background-color: $background;
+}
+</style>
