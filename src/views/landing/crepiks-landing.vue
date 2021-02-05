16efced945715843c586685a44a2beaa5a36028e
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2021-02-06 01:00:17 +0600

    feat: create description block

--- src/views/landing/crepiks-landing.vue
+++ src/views/landing/crepiks-landing.vue
@@ -2,17 +2,20 @@
   <div class="landing">
     <c-header />
     <cIntro />
+    <cDescription />
   </div>
 </template>
 
 <script>
 import cHeader from "@/components/new-landing/crepiks-header.vue";
 import cIntro from "@/components/new-landing/crepiks-intro.vue";
+import cDescription from "@/components/new-landing/crepiks-description.vue";
 
 export default {
   components: {
     cHeader,
-    cIntro
+    cIntro,
+    cDescription
   }
 };
 </script>
