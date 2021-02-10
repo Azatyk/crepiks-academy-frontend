eab38cd5d081f905ab14c33ff004a02396319df0
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2021-02-10 21:45:48 +0600

    feat add links to socials

--- src/components/landing/crepiks-questions.vue
+++ src/components/landing/crepiks-questions.vue
@@ -3,6 +3,74 @@
     <div class="questions-content">
       <h2 class="questions-heading">Остались вопросы?</h2>
       <cButton text="Спросить" class="questions-button" />
+      <div class="pay__slide-links">
+        <a
+          href="https://wa.me/77078139703"
+          class="pay__slide-link pay__slide-link-whatsapp"
+          target="_blank"
+        >
+          <img
+            src="@/assets/icons/whatsapp.png"
+            alt="WhatsApp"
+            class="pay__slide-link-icon"
+          />
+          WhatsApp
+        </a>
+        <a
+          href="https://t.me/CrepiksAcademyBot"
+          class="pay__slide-link pay__slide-link-telegram"
+          target="_blank"
+        >
+          <img
+            src="@/assets/icons/telegram.png"
+            alt="Telegram"
+            class="pay__slide-link-icon"
+          />
+          Telegram
+        </a>
+        <a
+          href="https://www.instagram.com/crepiks_academy/"
+          class="pay__slide-link pay__slide-link-instagram"
+          target="_blank"
+        >
+          <img
+            src="@/assets/icons/instagram.png"
+            alt="Instagram"
+            class="pay__slide-link-icon"
+          />
+          Instagram
+        </a>
+        <a
+          href="mailto:crepiks.academy@mail.ru"
+          class="pay__slide-link pay__slide-link-email"
+          target="_blank"
+        >
+          <img
+            src="@/assets/icons/email.png"
+            alt="Почта"
+            class="pay__slide-link-icon"
+          />
+          Почта
+        </a>
+        <a
+          href="https://vk.com/public200320088"
+          class="pay__slide-link pay__slide-link-vkontakte"
+          target="_blank"
+        >
+          <img
+            src="@/assets/icons/vkontakte.png"
+            alt="Telegram"
+            class="pay__slide-link-icon"
+          />
+          Вконтакте
+        </a>
+        <a
+          href="tel:87078139703"
+          class="pay__slide-link pay__slide-link-number"
+        >
+          8 (707) 813 97 03
+        </a>
+      </div>
     </div>
   </div>
 </template>
