bfeca74f40864c146ab6bcf09162bc4377cfec7f
Donchidzee <47913017+Donchidzee@users.noreply.github.com>
2021-02-15 18:26:54 +0600

    refactor: decomposite profile edit and change password functions in separate components

--- src/views/profile/crepiks-profile.vue
+++ src/views/profile/crepiks-profile.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="user-profile">
-    <notification
-      :isActive="isNotificationOpen"
-      :heading="notificationHeading"
-      :text="notificationText"
-      @close-notification="isNotificationOpen = false"
-      :status="notificationStatus"
-    />
     <div class="user-profile-info">
       <div class="user-profile-image">
         <img
@@ -62,76 +55,14 @@
         />
       </div>
     </div>
-    <rightSideBlock
-      :isOpen="openProfileEdit"
-      @close-block="openProfileEdit = false"
-    >
-      <div class="right-block">
-        <h2 class="right-block-heading">Редактирование профиля</h2>
-        <div class="right-block-container">
-          <cInput
-            class="right-block-input"
-            title="Имя"
-            v-model="updatedUser.firstName"
-            :maxlength="maxlength"
-          />
-          <cInput
-            class="right-block-input"
-            title="Фамилия"
-            v-model="updatedUser.lastName"
-            :maxlength="maxlength"
-          />
-          <cInput
-            class="right-block-input"
-            title="Почта"
-            type="email"
-            v-model="updatedUser.email"
-            :maxlength="maxlength"
-          />
-          <cButton
-            class="right-block-button"
-            text="Изменить"
-            @click="changeUserData"
-          />
-        </div>
-      </div>
-    </rightSideBlock>
-    <rightSideBlock
-      :isOpen="openChangePassword"
-      @close-block="openChangePassword = false"
-    >
-      <div class="right-block">
-        <h2 class="right-block-heading">Изменение пароля</h2>
-        <div class="right-block-container">
-          <cInput
-            class="right-block-input"
-            title="Старый пароль"
-            v-model="passwords.currentPassword"
-            placeholder="Введите старый пароль"
-            type="password"
-          />
-          <cInput
-            class="right-block-input"
-            title="Новый пароль"
-            v-model="passwords.newPassword"
-            placeholder="Введите новый пароль"
-            type="password"
-          />
-          <cInput
-            class="right-block-input"
-            title="Повторите новый пароль"
-            v-model="passwords.newPasswordCheck"
-            placeholder="Повторите новый пароль"
-            type="password"
-          />
-          <cButton
-            class="right-block-button"
-            text="Изменить"
-            @click="changePassword"
-          />
-        </div>
-      </div>
-    </rightSideBlock>
+    <profileEdit
+      :isProfileEditOpen="openProfileEdit"
+      @close-profile-block="openProfileEdit = false"
+    />
+    <changePassword
+      :isOpenChangePassword="openChangePassword"
+      @close-change-password-block="openChangePassword = false"
+    />
   </div>
 </template>
 
@@ -141,21 +72,16 @@ import courseCard from "@/components/courses/crepiks-course-card";
 import firstCourseImage from "@/assets/images/basic-markup-image-small.png";
 import secondCourseImage from "@/assets/images/pro-markup-image-small.png";
 
-import rightSideBlock from "@/components/common/crepiks-right-side-block";
-import cInput from "@/components/common/crepiks-input";
-import cButton from "@/components/common/crepiks-button";
-
-import notification from "@/components/common/crepiks-notification";
+import profileEdit from "@/components/profile/crepiks-profile-edit";
+import changePassword from "@/components/profile/crepiks-change-password";
 
 import { mapGetters } from "vuex";
 
 export default {
   components: {
     courseCard,
-    rightSideBlock,
-    cInput,
-    cButton,
-    notification
+    profileEdit,
+    changePassword
   },
   data() {
     return {
@@ -163,92 +89,11 @@ export default {
       secondCourseImage: secondCourseImage,
       openProfileEdit: false,
       openChangePassword: false,
-      maxlength: 34,
-      isNotificationOpen: false,
-      notificationHeading: "",
-      notificationText: "",
-      notificationStatus: "",
       user: {
         firstName: null,
-        lastName: null,
-        email: null
-      },
-      updatedUser: {
-        firstName: null,
-        lastName: null,
-        email: null
-      },
-      passwords: {
-        currentPassword: null,
-        newPassword: null,
-        newPasswordCheck: null
-      },
-      userId: null
-    };
-  },
-  methods: {
-    changeUserData() {
-      this.openProfileEdit = false;
-
-      let updatedData = {
-        firstName: this.updatedUser.firstName,
-        lastName: this.updatedUser.lastName,
-        email: this.updatedUser.email
-      };
-
-      let id = this.userId;
-      this.$store
-        .dispatch("changeUserData", { id, updatedData })
-        .then(() => {
-          this.isNotificationOpen = true;
-          this.notificationStatus = "success";
-          (this.notificationHeading = "Профиль сохранен"),
-            (this.notificationText = "Ваши данные изменены");
-        })
-        .catch(() => {
-          this.isNotificationOpen = true;
-          this.notificationStatus = "error";
-          (this.notificationHeading = "Ошибка"),
-            (this.notificationText = "Не удалось редактировать профиль");
-        });
-    },
-
-    changePassword() {
-      if (
-        this.passwords.newPassword.trim() !==
-        this.passwords.newPasswordCheck.trim()
-      ) {
-        this.isNotificationOpen = true;
-        this.notificationStatus = "error";
-        (this.notificationHeading = "Ошибка"),
-          (this.notificationText = "Не удалось изменить пароль");
-      } else {
-        let passwords = {
-          oldPassword: this.passwords.currentPassword,
-          newPassword: this.passwords.newPassword
-        };
-
-        let id = this.userId;
-        this.$store
-          .dispatch("changePassword", { id, passwords })
-          .then(() => (this.isChangePasswordLoading = false))
-          .then(() => {
-            this.openChangePassword = false;
-            this.isNotificationOpen = true;
-            this.notificationStatus = "success";
-            (this.notificationHeading = "Пароль изменен"),
-              (this.notificationText =
-                "Вы удачно изменили пароль, используйте его для входа");
-          })
-          .catch(() => {
-            this.isNotificationOpen = true;
-            this.notificationStatus = "error";
-            (this.notificationHeading = "Неверный пароль"),
-              (this.notificationText =
-                "Вы неверно ввели текущий пароль, попробуйте снова");
-          });
+        lastName: null
       }
-    }
+    };
   },
   computed: {
     ...mapGetters(["userData"])
@@ -256,26 +101,12 @@ export default {
   mounted() {
     if (this.userData) {
       this.user = this.userData;
-      this.updatedUser.firstName = this.userData.firstName;
-      this.updatedUser.lastName = this.userData.lastName;
-      this.updatedUser.email = this.userData.email;
-      this.userId = this.user.id;
     }
   },
 
   watch: {
     userData() {
       this.user = this.userData;
-      this.updatedUser.firstName = this.userData.firstName;
-      this.updatedUser.lastName = this.userData.lastName;
-      this.updatedUser.email = this.userData.email;
-    },
-    openChangePassword() {
-      if (this.openChangePassword) {
-        this.passwords.currentPassword = null;
-        this.passwords.newPassword = null;
-        this.passwords.newPasswordCheck = null;
-      }
     }
   }
 };
@@ -385,36 +216,11 @@ export default {
   }
 }
 
-.right-block {
-  padding: 30px 0;
-  box-sizing: border-box;
-
-  &-heading {
-    font-weight: 500;
-    font-size: 32px;
-    margin-bottom: 30px;
-  }
-
-  &-input {
-    margin-top: 20px;
-  }
-
-  &-button {
-    margin-top: 40px;
-  }
-}
-
 @media (max-width: 1244px) {
   .user-profile {
     padding: 100px 5%;
     padding-top: 100px;
     width: 830px;
-  }
-
-  .right-block {
-    &-heading {
-      font-size: 27px;
-    }
   }
 }
 
@@ -453,12 +259,6 @@ export default {
       }
     }
   }
-
-  .right-block {
-    &-heading {
-      font-size: 24px;
-    }
-  }
 }
 
 @media (max-width: 525px) {
@@ -473,12 +273,6 @@ export default {
         padding-top: 30px;
         margin-left: 0;
       }
-    }
-  }
-
-  .right-block {
-    &-heading {
-      font-size: 20px;
     }
   }
 }
